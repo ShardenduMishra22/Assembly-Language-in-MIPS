f3f932e80c55dc141877a21ba492e415ab44905f
Shardendu Mishra <shardendumishra01@gmail.com>
2024-11-10 16:52:40 +0530

    MIPS

--- 1.asm
+++ 1.asm
@@ -1,5 +1,3 @@
-# Store 32b in $t0, $t1 and store in $t2
-
 .data
 x : .word 10
 y : .word -11
@@ -7,19 +5,19 @@ y : .word -11
 .text
 
 .globl main
-main:
+main:                # Define the main label here once
 
-la $t2,x
-lw $t0,0($t2)
+la $t2, x           # Load address of x into $t2
+lw $t0, 0($t2)      # Load value of x into $t0
 
-la $t2,y
-lw $t1,0($t2)
+la $t2, y           # Load address of y into $t2
+lw $t1, 0($t2)      # Load value of y into $t1
 
-add $t2,$t1,$t0
+add $t2, $t1, $t0   # Add $t0 and $t1 and store result in $t2
 
-li $v0, 10
-add $a0, $t2, $t0
-syscall
+li $v0, 10          # Prepare for program exit
+add $a0, $t2, $t0   # Add $t2 and $t0, store in $a0
+syscall             # Exit the program
 
-li $v0, 10
+li $v0, 10          # Exit program
 syscall
